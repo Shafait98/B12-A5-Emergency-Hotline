8f33b5969bda7fbd3b6729f7c05bf8f83d454a64
Shafait98 <farhan98aiub@gmail.com>
2025-08-29 23:06:42 +0600

    js code added

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const callHistoryWrapper    = document.getElementById('call-history-wrapper');
+const heartCount            = document.getElementById('heart-count');
+const copyCount             = document.getElementById('copy-count');
+
+const callBtns              = document.querySelectorAll('.call-btn');
+const historyClearBtn       = document.getElementById('history-clear-btn');
+const heartBtns             = document.querySelectorAll('.heart-btn');
+const copyBtns              = document.querySelectorAll('.copy-btn');
